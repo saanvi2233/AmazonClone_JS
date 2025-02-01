a405aac5a44028f72e5a05191583ed3f3aea0eb7
saanvi2233 <saanvigupta2233@gmail.com>
2025-02-01 15:22:05 +0530

    data file added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,24 +1,31 @@
-const products = [
-    {
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: { stars: 4.5, count: 87 },
-        priceCents: 1090
-    },
-    {
-        image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: { stars: 4, count: 127 },
-        priceCents: 2095
-    },
-    {
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: { stars: 4.5, count: 56 },
-        priceCents: 799
-    }
-];
+// const products = [
+//     {
+//         image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//         name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//         rating: { stars: 4.5, count: 87 },
+//         priceCents: 1090
+//     },
+//     {
+//         image: 'images/products/intermediate-composite-basketball.jpg',
+//         name: 'Intermediate Size Basketball',
+//         rating: { stars: 4, count: 127 },
+//         priceCents: 2095
+//     },
+//     {
+//         image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//         name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//         rating: { stars: 4.5, count: 56 },
+//         priceCents: 799
+//     },
+//     {
+//         image: 'images/products/black-2-slot-toaster.jpg',
+//         name: 'Black 2-Slot Toaster',
+//         rating: { stars: 3.5, count: 34 },
+//         priceCents: 2499
+//     }
+// ];
 
+// now all the products come for the data folder
 let productHTML = '';
 
 products.forEach(product => {
